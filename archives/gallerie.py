d0db043e88820d4a9fe685a8f8e7dcce2a869223
ChGrenier <greniercharline@hotmail.fr>
2019-12-19 20:57:30 +0100

    lazy loading

--- archives/gallerie.py
+++ archives/gallerie.py
@@ -1,14 +1,14 @@
 import os
 from os.path import join, getsize
 
-nom_dossier = 'bruxelles'
+nom_dossier = 'billund'
 sortie = open('sortie.html', 'w')
 
 for root, dirs, files in os.walk('../voyages/images/'+nom_dossier, topdown=True):
     files.sort()
     # print(files[:5])
     for fichier in files :
-        sortie.write('<a href="../images/'+ nom_dossier +'/'+ fichier +'" data-lightbox="roadtrip"><img src="../images/'+ nom_dossier +'/'+ fichier +'" alt="" height="auto" width="30%"/></a>'+'\n')
+        sortie.write('<a href="../images/'+ nom_dossier +'/'+ fichier +'" data-lightbox="roadtrip"><img data-src="../images/'+ nom_dossier +'/'+ fichier +'" alt="" height="auto" width="30%"uk-img></a>'+'\n')
 
     # print(root, "consumes", end=" ")
     # print(sum(getsize(join(root, name)) for name in files), end=" ")
@@ -17,3 +17,5 @@ for root, dirs, files in os.walk('../voyages/images/'+nom_dossier, topdown=True)
     #     dirs.remove('CVS')  # don't visit CVS directories
 
 sortie.close()
+
+# <img data-src="" width="" height="" alt="" uk-img>
