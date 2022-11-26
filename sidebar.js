659cff94dbb0c38ac14acd7528ffe0c825b9ee1a
ChGrenier <greniercharline@hotmail.fr>
2022-11-26 21:14:14 +0100

    resume et articles

--- sidebar.js
+++ sidebar.js
@@ -3,13 +3,6 @@ document.write('\
                 <div id="sidebar">\
                     <div class="inner">\
 \
-                        <!-- Search -->\
-                            <section id="search" class="alt">\
-                                <form method="post" action="#">\
-                                    <input type="text" name="query" id="query" placeholder="Search" />\
-                                </form>\
-                            </section>\
-\
                         <!-- Menu -->\
                             <nav id="menu">\
                                 <header class="major">\
@@ -17,70 +10,27 @@ document.write('\
                                 </header>\
                                 <ul>\
                                     <li><a href="index.html">Homepage</a></li>\
-                                    <li><a href="resume.html">CV</a></li>\
-                                    <li><a href="generic.html">Generic</a></li>\
-                                    <li><a href="elements.html">Elements</a></li>\
+                                    <li><a href="resume.html">Resume</a></li>\
                                     <li>\
-                                        <span class="opener">Submenu</span>\
+                                        <span class="opener">Publications</span>\
                                         <ul>\
-                                            <li><a href="#">Lorem Dolor</a></li>\
-                                            <li><a href="#">Ipsum Adipiscing</a></li>\
-                                            <li><a href="#">Tempus Magna</a></li>\
-                                            <li><a href="#">Feugiat Veroeros</a></li>\
+                                            <li><a href="articles.html">Articles</a></li>\
+                                            <li><a href="#">Presentations</a></li>\
                                         </ul>\
                                     </li>\
-                                    <li><a href="#">Etiam Dolore</a></li>\
-                                    <li><a href="#">Adipiscing</a></li>\
-                                    <li>\
-                                        <span class="opener">Another Submenu</span>\
-                                        <ul>\
-                                            <li><a href="#">Lorem Dolor</a></li>\
-                                            <li><a href="#">Ipsum Adipiscing</a></li>\
-                                            <li><a href="#">Tempus Magna</a></li>\
-                                            <li><a href="#">Feugiat Veroeros</a></li>\
-                                        </ul>\
-                                    </li>\
-                                    <li><a href="#">Maximus Erat</a></li>\
-                                    <li><a href="#">Sapien Mauris</a></li>\
-                                    <li><a href="#">Amet Lacinia</a></li>\
+                                    <li><a href="archive_generic.html">Generic</a></li>\
+                                    <li><a href="archive_elements.html">Elements</a></li>\
                                 </ul>\
                             </nav>\
 \
                         <!-- Section -->\
                             <section>\
                                 <header class="major">\
-                                    <h2>Ante interdum</h2>\
+                                    <h2>Contact</h2>\
                                 </header>\
-                                <div class="mini-posts">\
-                                    <article>\
-                                        <a href="#" class="image"><img src="images/pic07.jpg" alt="" /></a>\
-                                        <p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore aliquam.</p>\
-                                    </article>\
-                                    <article>\
-                                        <a href="#" class="image"><img src="images/pic08.jpg" alt="" /></a>\
-                                        <p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore aliquam.</p>\
-                                    </article>\
-                                    <article>\
-                                        <a href="#" class="image"><img src="images/pic09.jpg" alt="" /></a>\
-                                        <p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore aliquam.</p>\
-                                    </article>\
-                                </div>\
-                                <ul class="actions">\
-                                    <li><a href="#" class="button">More</a></li>\
-                                </ul>\
-                            </section>\
-\
-                        <!-- Section -->\
-                            <section>\
-                                <header class="major">\
-                                    <h2>Get in touch</h2>\
-                                </header>\
-                                <p>Sed varius enim lorem ullamcorper dolore aliquam aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin sed aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>\
                                 <ul class="contact">\
-                                    <li class="icon solid fa-envelope"><a href="#">information@untitled.tld</a></li>\
-                                    <li class="icon solid fa-phone">(000) 000-0000</li>\
-                                    <li class="icon solid fa-home">1234 Somewhere Road #8254<br />\
-                                    Nashville, TN 00000-0000</li>\
+                                    <li class="icon solid fa-envelope"><a href="#">charline.grenier@unistra.fr</a></li>\
+                                    <li class="icon brands fa-github"><a href="#">ChGrenier</a></li>\
                                 </ul>\
                             </section>\
 \
